94a79ea5fb34bf6cdef3aa4e3e66ef1a2f189712
beatriz anjo <bea.moaraesanjo@gmail.com>
2026-03-24 21:57:58 -0300

    alterando arquivos Cabecalho.tsx

--- react/src/componets/Cabecalho.tsx
+++ react/src/componets/Cabecalho.tsx
@@ -4,7 +4,7 @@ function Cabecalho() {
     return(
         <div>
         <h1> corno é gay </h1>
-        <h1> hetero é manco</h1>
+        <h1> hetero           é manco</h1>
         </div>
     )
 }
