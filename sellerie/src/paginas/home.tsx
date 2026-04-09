6d917f79bcb4b1d4b8fa2dc9a9dfd22452f054b5
miibertaglia <miihletoo@gmail.com>
2026-04-08 22:01:37 -0300

    Iniciei a Home

--- sellerie/src/paginas/home.tsx
+++ sellerie/src/paginas/home.tsx
@@ -1,6 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>Home</h1>
+    <p>Bem-vindo à página inicial!</p>
+
+    <main> 
+        <section className="banner"> 
+            <img src= "https://i.pinimg.com/1200x/19/92/e5/1992e587b26d2d9b153e0169c1139a6e.jpg"></img>
+        </section>
+    </main>
+    
+</body>
+</html>
+
+
+
+
 function Home() {
     return (
-        <div>             
+        <div> 
+
         </div>
     )
 }
