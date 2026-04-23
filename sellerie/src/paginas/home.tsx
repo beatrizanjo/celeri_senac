679317243268603bdf16cdafff2da544696ff6de
miibertaglia <miihletoo@gmail.com>
2026-04-22 22:20:29 -0300

    atualizando home

--- sellerie/src/paginas/home.tsx
+++ sellerie/src/paginas/home.tsx
@@ -13,6 +13,14 @@ function Home() {
                 <section className="banner">
                     <img src="home.png"  alt="imagem de banner" />
                 </section>
+
+                <div>
+                    <section className="massascarnessobremesa">
+                        <img className="massas" src= "Massas.png" alt="imagem de sobremesas" />
+                        <img className="carnes" src= "Carnes.png" alt="imagem de carnes" />
+                        <img className="sobremesa" src= "Sobremesa.png" alt="imagem de sobremesas" />
+                    </section>
+                </div>
             
             
         </div>
