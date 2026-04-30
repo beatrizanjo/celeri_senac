1af4ab084ae83f63cd1b6eead5919fea41ca87a0
miibertaglia <miihletoo@gmail.com>
2026-04-29 21:55:11 -0300

    ajustes rodape

--- sellerie/src/componentes/rodape/index.tsx
+++ sellerie/src/componentes/rodape/index.tsx
@@ -1,13 +1,13 @@
-
+import "../../style/style.css"
 function Rodape() {
 
     return (
 
         <div className='rodape'>
-            <h1>Rodape</h1>
+            
             <h2>Siga-nos </h2>
             <h3>Politica de Privacidade</h3>
-            <img src="facebook.png" width="50" height="50" alt="Facebook" />
+            <img src="facebook.png" width="50" height="50" alt="Facebook" aling-item="center" />
             <img src="instagram.png" width="50" height="50" alt="Instagram" />
             <img src="youtube.png" width="50" height="50" alt="YouTube" />
         </div>
